c0a5677b2cd71255b35379621722fb8d6d8dd160
FichteFoll <fichtefoll2@googlemail.com>
2023-10-26 00:29:01 +0200

    Convert Key component to composition API & TypeScript
    Because I felt like it …

--- docs/.vitepress/components/Key.vue
+++ docs/.vitepress/components/Key.vue
@@ -2,7 +2,9 @@
   <span class="kbd-container" v-html="keysHtml" />
 </template>
 
-<script>
+<script lang="ts">
+import { computed } from 'vue';
+
 const TRANSLATION_MAP = new Map([
   // https://www.sublimetext.com/docs/key_bindings.html#keys
   // http://xahlee.info/comp/unicode_computing_symbols.html
@@ -43,50 +45,55 @@ const TRANSLATION_MAP = new Map([
 
 const MODIFIER_ORDER = ['super', 'ctrl', 'alt', 'altgr', 'option', 'shift', 'command'];
 
-export default {
-  props: {
-    k: String,
-  },
-  computed: {
-    chords() {
-      return this.k.trim()
-        .split(/\s*\,(?!$)\s*/)
-        .map(this.parseChord);
-    },
-    keysHtml() {
-      return this.chords
-        .map(chord => {
-          const kbds = chord
-            .map(this.translateKey)
-            .map(key => `<kbd>${key}</kbd>`)
-            .join("&nbsp;");
-          return `<span title="${chord.join("+")}">${kbds}</span>`;
-        })
-        .join(", ");
-    }
-  },
-  methods: {
-    parseChord(chordString) {
-      const chord = chordString.trim().split(/\s*\+(?!$)\s*/);
-      this.sortModifiers(chord);
-      return chord;
-    },
-    translateKey(key) {
-      if (TRANSLATION_MAP.has(key)) {
-        return TRANSLATION_MAP.get(key);
-      } else if (key.startsWith("keypad")) {
-        return key.substring(6) + " (Keypad)";
-      } else {
-        return key.charAt(0).toUpperCase() + key.slice(1);
-      }
-    },
-    modifierIndex(modifier) {
-      const index = MODIFIER_ORDER.indexOf(modifier);
-      return index !== -1 ? index : MODIFIER_ORDER.length;
-    },
-    sortModifiers(chord) {
-      chord.sort((a, b) => this.modifierIndex(a) - this.modifierIndex(b));
-    },
-  },
+type Chord = Array<string>;
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  k: string,
+}>();
+
+const chords = computed(() =>
+  props.k.trim()
+    .split(/\s*\,(?!$)\s*/)
+    .map(parseChord),
+);
+
+function parseChord(chordString: string): Chord {
+  const chord = chordString.trim().split(/\s*\+(?!$)\s*/);
+  sortModifiers(chord);
+  return chord;
+}
+
+// compute HTML directly because of NBSP usage
+const keysHtml = computed(() =>
+  chords.value
+    .map(chord => {
+      const kbds = chord
+        .map(translateKey)
+        .map(key => `<kbd>${key}</kbd>`)
+        .join("&nbsp;");
+      return `<span title="${chord.join("+")}">${kbds}</span>`;
+    })
+    .join(", "),
+);
+
+function translateKey(key: string): string {
+  if (TRANSLATION_MAP.has(key)) {
+    return TRANSLATION_MAP.get(key);
+  } else if (key.startsWith("keypad")) {
+    return key.substring(6) + " (Keypad)";
+  } else {
+    return key.charAt(0).toUpperCase() + key.slice(1);
+  }
+}
+
+function modifierIndex(modifier: string): number {
+  const index = MODIFIER_ORDER.indexOf(modifier);
+  return index !== -1 ? index : MODIFIER_ORDER.length;
+}
+
+function sortModifiers(chord: Chord): void {
+  chord.sort((a, b) => modifierIndex(a) - modifierIndex(b));
 }
 </script>
